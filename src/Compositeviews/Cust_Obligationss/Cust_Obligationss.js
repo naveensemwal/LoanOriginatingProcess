a039fe1eb3f0796e2c1ef4f898f022950426cd0e
amita123 <singhamita22may@gmail.com>
2020-03-12 11:28:26 +0530

    new tab added

--- src/Compositeviews/Cust_Obligationss/Cust_Obligationss.js
+++ src/Compositeviews/Cust_Obligationss/Cust_Obligationss.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
 export default class Cust_Obligationss extends Component{
-    
+    render(){
+        return(
+            alert('hi')
+        );
+    }
 }
